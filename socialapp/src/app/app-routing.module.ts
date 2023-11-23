5ede33b8ce6307f94a719a8a20fcc4530f19cb78
SameOldSteven <chtiouiaa25@gmail.com>
2023-11-23 18:17:14 +0100

    fixed routing

--- socialapp/src/app/app-routing.module.ts
+++ socialapp/src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile/profile.component'
 import { GroupComponent } from './groups/groups/groups.component';
 import { FriendsComponent } from './friends/friends/friends.component';
+import { FeedComponent } from './feed/feed/feed.component';
+import { ChatboxComponent } from './chatbox/chatbox/chatbox.component';
+import { ChatwindowComponent } from './chatwindow/chatwindow/chatwindow.component';
+import { RegisterComponent } from './register/register/register.component';
+import { LoginComponent } from './login/login/login.component';
 
-const routes: Routes = [
+ const routes: Routes = [
+  { path: '', redirectTo: '/feed', pathMatch: 'full' },
+  { path: 'feed', component: FeedComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'groups', component: GroupComponent },
-  { path: 'friends', component: FriendsComponent },
-  // other routes...
+  { path: 'friends', component: FriendsComponent},
+  { path: 'chatbox', component: ChatboxComponent},
+  { path: 'chatwindow', component: ChatwindowComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
